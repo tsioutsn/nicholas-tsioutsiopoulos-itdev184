9c5a78420e6837f78aefbd84ecd6ebc9ae6721a4
tsioutsn <61215311+tsioutsn@users.noreply.github.com>
2021-02-22 19:09:26 -0800

    Add files via upload

---
+++ Activity-2/MyFirstApp/MyFirstApp/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  MyFirstApp
+//
+//  Created by Nikko on 2/22/21.
+//  Copyright © 2021 Nikko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+       
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
