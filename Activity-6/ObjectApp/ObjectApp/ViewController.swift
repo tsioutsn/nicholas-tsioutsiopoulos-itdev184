1b3e3bffa43d46161f9fe7e9e26854f89d680e95
tsioutsn <61215311+tsioutsn@users.noreply.github.com>
2021-03-04 19:14:08 -0800

    Add files via upload

---
+++ Activity-6/ObjectApp/ObjectApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ObjectApp
+//
+//  Created by Nikko on 3/4/21.
+//  Copyright © 2021 Nikko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
