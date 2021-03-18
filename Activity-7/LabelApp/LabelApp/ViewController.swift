8082743431d0d03580604ccbd73c28a8711ed3f4
tsioutsn <61215311+tsioutsn@users.noreply.github.com>
2021-03-18 12:32:32 -0700

    Add files via upload

---
+++ Activity-7/LabelApp/LabelApp/ViewController.swift
@@ -0,0 +1,34 @@
+//
+//  ViewController.swift
+//  LabelApp
+//
+//  Created by Nikko on 3/15/21.
+//  Copyright © 2021 Nikko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var myLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func changeLabel(_ sender: UIButton) {
+        myLabel.text = "Text created by Swift \ncode using Xcode"
+        myLabel.numberOfLines = 2
+        myLabel.font = UIFont(name: "Courier", size: 14)
+        myLabel.backgroundColor = UIColor.yellow
+        myLabel.isEnabled = false
+    }
+    
+}
+
